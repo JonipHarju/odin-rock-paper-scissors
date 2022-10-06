24182cad798281ff5fedc578e84893b30c3ae51d
joni <jonipharju@icloud.com>
2022-10-06 11:00:38 +0300

    wrote computers rock, paper or scissor function

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
 console.log("Hi!");
+
+const options = ["Rock", "Paper", "Scissors"];
+// Function for the computer to draw rock, paper or scissor
+function getComputerChoice() {
+  const computerChoice = options[Math.floor(Math.random() * options.length)];
+  return computerChoice;
+}
